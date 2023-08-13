087bd8b80d78bf31d335e019928b2ccb191cfba0
AlainJoss <alain.joss.1998@gmail.com>
2023-08-13 09:01:56 +0200

    Working version of app, ready for optimizing look.

--- app/app.py
+++ app/app.py
@@ -1,44 +1,59 @@
 import streamlit as st
 import resolutor
-import os
 import cv2
 
+if 'original' not in st.session_state:
+    st.session_state.original = None
+
+if 'upscaled' not in st.session_state:
+    st.session_state.upscaled = None
+
+if 'succeded' not in st.session_state:
+    st.session_state.succeded = None
+
 st.title("Super-Resolutor")
-
 st.write("Super resolutor is a super-hero which enables you to x4 the resolution of your images.")
-
 st.write("### Upload Image")
 
 uploaded_file = st.file_uploader(label="Upload your image", type=['png', 'jpg'])
 
 if uploaded_file is not None:
-    with st.spinner("Upscaling in progress ..."):
-        try:
-            opencv_image = resolutor.file_to_opencv_image(uploaded_file)
 
-            upscaled = resolutor.upscale(opencv_image)
+    opencv_image = resolutor.file_to_opencv_image(uploaded_file)
+    st.session_state.original = opencv_image
 
-            st.write("### Comparison")
+    with st.form("upscale", clear_on_submit=True):
+        if st.form_submit_button("Upscale"):
+            with st.spinner("Upscaling in progress ..."):
+                try:
+                    upscaled = resolutor.upscale(opencv_image)
+                    st.session_state.upscaled = upscaled
+                    st.session_state.succeded = True
 
-            col1, col2 = st.columns(2)
+                    st.write("### Comparison")
 
-            with col1: 
-                st.write("Original image")
-                st.image(opencv_image)
-            with col2:
-                st.write("Upscaled image")
-                st.image(upscaled)
+                    col1, col2 = st.columns(2)
 
-            # Convert numpy array to binary data
-            _, buffer = cv2.imencode('.png', upscaled)
-            binary_data = buffer.tobytes()
+                    with col1: 
+                        st.write("Original image")
+                        st.image(st.session_state.original)
+                    with col2:
+                        st.write("Upscaled image")
+                        st.image(st.session_state.upscaled)
 
-            st.write("### Download")
+                except:
+                    st.write("An error has occured. It may be that the image has a too high resolution already.")
 
-            st.download_button(
-                label='Upscaled Image',
-                file_name='upscaled.png',
-                data=binary_data
-            )
-        except:
-            st.write("An error has occured. It may be that the image has a too high resolution already.")
+    if st.session_state.succeded:
+        # Convert numpy array to binary data
+        upscaled_rgb = cv2.cvtColor(st.session_state.upscaled, cv2.COLOR_BGR2RGB)
+        _, buffer = cv2.imencode('.png', upscaled_rgb)
+        binary_data = buffer.tobytes()
+
+        st.write("### Download")
+
+        st.download_button(
+            label='Upscaled Image',
+            file_name='upscaled.png',
+            data=binary_data
+        )
